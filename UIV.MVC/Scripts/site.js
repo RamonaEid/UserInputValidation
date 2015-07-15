7d0331efd23db9a77b6279646a9431428a23fb5b
Ramona Eid <eid@intermark.com>
2015-07-15 17:01:44 -0600

    Added a third UI called UIV.MVC and refactored

--- UIV.MVC/Scripts/site.js
+++ UIV.MVC/Scripts/site.js
@@ -10,7 +10,6 @@
 
     var resultDisplayText = $("#resultDisplay").html().toString();
     if (resultDisplayText.indexOf("Oops") == -1) {
-        // text-success lead
         if (resultDisplayText.indexOf("Bingo") == -1)
             $("#resultDisplay").attr("class", "text-winner lead");
         else
